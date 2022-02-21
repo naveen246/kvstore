09a4b1ecea20f17fbd977a224d3cea6e204922eb
Naveen <naveen246@gmail.com>
2022-02-21 08:31:30 +0530

    Add snapshot args and reply

---
+++ src/raft/raft_snapshot.go
@@ -0,0 +1,26 @@
+package raft
+
+// InstallSnapshotArgs Figure 13 from Raft paper
+type InstallSnapshotArgs struct {
+	Term              int
+	LeaderId          int
+	LastIncludedIndex int
+	LastIncludedTerm  int
+	Data              []byte
+	Done              bool
+}
+
+type InstallSnapshotReply struct {
+	Term int
+}
+
+func (rf *Raft) sendInstallSnapshot(server int, args InstallSnapshotArgs, reply *InstallSnapshotReply) bool {
+	ok := rf.peers[server].Call("Raft.InstallSnapshot", args, reply)
+	return ok
+}
+
+func (rf *Raft) InstallSnapshot(args *InstallSnapshotArgs, reply *InstallSnapshotReply) {
+	rf.lockMutex()
+	defer rf.unlockMutex()
+
+}
